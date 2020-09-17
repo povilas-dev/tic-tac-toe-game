70c5935a0ca2545fc7c81945ec90dd198339cbb2
povilastwix <povilast@wix.com>
2020-09-17 13:47:56 +0300

    wix-style-react-errors

--- tic-tac-toe-ts/src/App.tsx
+++ tic-tac-toe-ts/src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Modal } from 'wix-style-react';
-import './App.css';
+import React, { useState } from "react";
+import Modal from "wix-style-react/dist/es/src/Modal";
+import Button from "wix-style-react/dist/es/src/Button";
+import "./App.css";
 
-const App: React.FunctionComponent<{}> = () =>{
+const App: React.FunctionComponent<{}> = () => {
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
   return (
     <div className="App">
+      <Button onClick={() => setIsModalVisible(!isModalVisible)} />
+      <Modal isOpen={isModalVisible}></Modal>
     </div>
   );
-}
+};
 
 export default App;
